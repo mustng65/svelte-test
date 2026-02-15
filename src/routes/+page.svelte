61ba29d75575ebd2e17195d98e05907bbd852f61
David <mustng65@hotmail.com>
2026-02-15 07:20:58 +0000

    basic changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+import Test from './test.svelte'
+</script>
+
+<div class="p-20">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Test name="Joe" ></Test>
+</div>
---
+++ src/routes/test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+let {name}=$props();
+</script>
+
+<h2>this is something {name}</h2>
+
+<button class="bg-sky-500 hover:bg-sky-700 p-4 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-500">Do something</button>
